4167ed6b7a049c0c66bc113f851056d6396d16f6
Solrac100 <carlost_100@hotmail.com>
2017-11-16 19:42:46 -0600

    Login-Falta Session

---
+++ App/Conexion/conexion.php
@@ -0,0 +1,17 @@
+<?php
+	$hostBD='localhost';
+	$userBD='root';
+	$passBD='asd';
+	$BD='horarios';
+
+	$conexionBD = mysql_connect($hostBD,$userBD,$passBD)
+		or die('No se pudo conectar al servidor ERROR:'.mysql_error());
+	mysql_select_db($BD) or die ('No se pudo abrir la BD ERROR:'.mysql_error());
+
+	$admin='A';
+	$jefe='J';
+	$maestro='M';
+	$prefecto='P';
+
+	session_start();
+?>
---
+++ App/login.php
@@ -0,0 +1,26 @@
+<?php
+	include("./Conexion/conexion.php");
+
+	$user = $_POST['user'];
+	$pass = $_POST['pass'];	
+
+	$consulta = "SELECT * FROM usuario WHERE usuario='".$user."' and contrasena='".$pass."';";
+  
+	$resultado = mysql_query($consulta) or die ("Error: ".mysql_error());
+	
+	$tablauser = mysql_fetch_array($resultado);
+	
+	$rol = $tablauser['rol']; 
+	
+	if($rol == $admin){
+		header("Location: ./admin");
+ 	}else if($rol == $jefe){
+ 		header("Location: ./jefe");
+ 	}else if($rol == $maestro){
+ 		header("Location: ./maestro");
+ 	}else if($rol == $prefecto){
+ 		header("Location: ./prefecto");
+ 	}else {
+ 		echo "No tiene permisos para acceder a la aplicacion. Consulte a un Administrador";
+ 	}
+?>
