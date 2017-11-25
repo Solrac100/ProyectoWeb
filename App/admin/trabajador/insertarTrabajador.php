91211475abcf9b620824887a59b87a83cda43269
Solrac100 <carlost_100@hotmail.com>
2017-11-25 15:44:10 -0600

    url image

--- App/admin/trabajador/insertarTrabajador.php
+++ App/admin/trabajador/insertarTrabajador.php
@@ -6,7 +6,16 @@ $nombre = $_POST['nombres'];
 $apellidos = $_POST['apellidos'];
 $celular =$_POST['telefono'];
 $correo =$_POST['correo'];
-$usuario =$_POST['foto'];
+
+if (is_uploaded_file($_FILES['foto']['tmp_name'])) {
+copy($_FILES['foto']['tmp_name'], '/var/www/ProyectoWeb/App/uploads'.$_FILES['foto']['name'].'');
+$subido = true;
+}
+if($subido) {
+echo "exito"; exit();
+} else {
+echo "Error"; exit();
+}
 
 $qry = "INSERT INTO trabajador(nombre,apellidos,celular,correo,usuario_idusuario) VALUES ('$nombre','$apellidos','$celular','$correo', '14')";
 $resultado = mysql_query($qry) or die ("*****ERROR AL INSERTAR TRABAJADOR: " .mysql_error());
